c7586f85fb3812b509eddd00f27db2d50367bb10
yusei-murai <yusei.murai@zozo.com>
2025-02-26 17:13:29 +0900

    コロンエラー追加

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,16 @@ const report: TextlintRuleModule = (context) => {
     return {
         [Syntax.CodeBlock](node) {
             const lang = node.lang || '';
-            if (!languages.includes(lang)) {
-                const ruleError = new RuleError(`不正なシンタックスハイライトのファイルタイプが見つかりました。:${lang}`);
-                report(node, ruleError);
+            if (languages.includes(lang)) {
+                return;
             }
+            if (lang.includes(":")) {
+                const ruleError = new RuleError(`はてなでは、コロン区切りでファイル名を記載することはできません。コード内に直接示すか、本文中に示してください。`);
+                report(node, ruleError);
+                return;
+            }
+            const ruleError = new RuleError(`不正なシンタックスハイライトのファイルタイプが見つかりました。:${lang}`);
+            report(node, ruleError);
         }
     }
 };
