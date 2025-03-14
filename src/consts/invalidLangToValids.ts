894311ddec78e5376c4fa83f5ccd1a31299b10ae
yusei-murai <yusei.murai@zozo.com>
2025-03-14 10:27:33 +0900

    リスト追加

--- src/consts/invalidLangToValids.ts
+++ src/consts/invalidLangToValids.ts
@@ -1,7 +1,10 @@
 export const invalidLangToValids: Map<string, string> = new Map([
     ['js', 'javascript'],
     ['bash', 'sh'],
+    ['shell', 'sh'],
     ['ts', 'typescript'],
     ['rb', 'ruby'],
     ['py', 'python'],
+    ['Makefile', 'make'],
+    ['VBScript', 'vb'],
 ]);
